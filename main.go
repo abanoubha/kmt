de6c439a315f25796d8914ce452b31259f95c738
Abanoub Hanna <abanoubhannaa73@gmail.com>
2023-10-14 06:37:19 +0300

    [feature] handle dummy command line arguments

--- main.go
+++ main.go
@@ -1,5 +1,25 @@
 package main
 
+import "os"
+
 func main() {
-	println("test")
+	if len(os.Args) < 2 {
+		println("help + run default")
+		return
+	}
+
+	switch os.Args[1] {
+	case "h", "help":
+		println("help")
+	case "v", "ver", "version":
+		println("version : 0.1.0")
+	case "build", "generate":
+		println("generate static webpages")
+	case "serve":
+		println("generate static webpages, then serve via localhost")
+	case "local", "localhost":
+		println("use localhost or 127.0.0.1 or 0.0.0.0")
+	default:
+		println("help + run default")
+	}
 }
